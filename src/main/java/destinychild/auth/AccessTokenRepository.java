3ccc07dbcc4695146ef94b69b24799140e66d8e9
magyariotto <magyariotto96@gmail.com>
2019-01-08 18:33:34 +0200

    first commit

---
+++ src/main/java/destinychild/auth/AccessTokenRepository.java
@@ -0,0 +1,17 @@
+package destinychild.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+
+public interface AccessTokenRepository extends JpaRepository<MsAccessToken, String> {
+    void deleteByUserId(String userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE MsAccessToken a WHERE a.lastAccess < :expiration AND a.persistent=false")
+    void deleteExpired(@Param("expiration") Long expiration);
+}
